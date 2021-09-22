0b2090a0bcc407f46c22a9745c82481d7ed03f34
dennyparas <dennyparas28@gmail.com>
2021-09-22 21:42:31 +0800

    add material ui package

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./features/home/HomePage";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Roboto", "Poppins"].join(","),
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
