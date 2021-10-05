d90363d2d3ce539cf2f83c0888751aeab84a8e9d
Daniel Paras <dennyparas28@gmail.com>
2021-10-06 06:19:40 +0800

    fix repo loadmore issue

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -13,7 +13,6 @@ const RepoList: React.FC<RepoListProps> = ({ repos, isLoading }) => {
   return (
     <Grid container spacing={2}>
       {repos.length > 0 &&
-        !isLoading &&
         repos.map((repo: Repo, index: number) => (
           <Grid key={index} item xs={12} sm={6} md={6} lg={4} xl={4}>
             <RepoCard repo={repo} />
